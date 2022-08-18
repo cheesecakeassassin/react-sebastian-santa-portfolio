699df2803cadc4984111e93067bfad47ef50e68a
cheesecakeassassin <santasebastian@yahoo.com>
2022-08-18 02:26:51 -0400

    Updates

---
+++ precache-manifest.166016827b812ce70da4d6b237cf3fc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6706a5cf1f095ce1b81a",
+    "url": "/react-sebastian-santa-portfolio/static/css/main.c097f0f4.chunk.css"
+  },
+  {
+    "revision": "6706a5cf1f095ce1b81a",
+    "url": "/react-sebastian-santa-portfolio/static/js/main.6706a5cf.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/react-sebastian-santa-portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "a9aff1eb85c8d94341c7",
+    "url": "/react-sebastian-santa-portfolio/static/js/runtime~main.a9aff1eb.js"
+  },
+  {
+    "revision": "52592dd2f33a2d11a6797e1a60d6b563",
+    "url": "/react-sebastian-santa-portfolio/index.html"
+  }
+];

1e71318562c9accce75172fba5c0f19a26143edd
cheesecakeassassin <santasebastian@yahoo.com>
2022-08-18 15:32:58 -0400

    Adding lichess svg, still needs work

---
+++ src/components/Header.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+export default class Header extends Component {
+    render() {
+        let resumeData = this.props.resumeData
+        const lichessLink = 'lichess'
+        return (
+            <React.Fragment>
+                <header id="home">
+                    <nav id="nav-wrap">
+                        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+                            Show navigation
+                        </a>
+                        <a className="mobile-btn" href="#" title="Hide navigation">
+                            Hide navigation
+                        </a>
+                        <ul id="nav" className="nav">
+                            <li className="current">
+                                <a className="smoothscroll" href="#home">
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a className="smoothscroll" href="#about">
+                                    About
+                                </a>
+                            </li>
+                            <li>
+                                <a className="smoothscroll" href="#resume">
+                                    Resume
+                                </a>
+                            </li>
+                            <li>
+                                <a className="smoothscroll" href="#portfolio">
+                                    Works
+                                </a>
+                            </li>
+                            <li>
+                                <a className="smoothscroll" href="#contact">
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <div className="row banner">
+                        <div className="banner-text">
+                            <h1 className="responsive-headline">I am {resumeData.name}.</h1>
+                            <h3 style={{ color: '#fff', fontFamily: 'sans-serif ' }}>
+                                I am a {resumeData.role}. {resumeData.roleDescription}
+                            </h3>
+                            <hr />
+                            <ul className="social">
+                                {resumeData.socialLinks &&
+                                    resumeData.socialLinks.map((item) => {
+                                        return (
+                                            <li key={item.name}>
+                                                <a href={item.url} target="_blank">
+                                                    {item.name === lichessLink ? (
+                                                        <svg className={item.className} viewBox="-0.692 0.5 51.573 52.285" xmlns="http://www.w3.org/2000/svg" width="22" height="22"><path d="M38.956.5c-3.53.418-6.452.902-9.286 2.984C5.534 1.786-.692 18.533.68 29.364 3.493 50.214 31.918 55.785 41.329 41.7c-7.444 7.696-19.276 8.752-28.323 3.084S-.506 27.392 4.683 17.567C9.873 7.742 18.996 4.535 29.03 6.405c2.43-1.418 5.225-3.22 7.655-3.187l-1.694 4.86 12.752 21.37c-.439 5.654-5.459 6.112-5.459 6.112-.574-1.47-1.634-2.942-4.842-6.036-3.207-3.094-17.465-10.177-15.788-16.207-2.001 6.967 10.311 14.152 14.04 17.663 3.73 3.51 5.426 6.04 5.795 6.756 0 0 9.392-2.504 7.838-8.927L37.4 7.171z" stroke="#FFF" fill='#FFF' stroke-linejoin="round"/></svg>
+                                                    ) : (
+                                                        <i className={item.className} />
+                                                    )}
+                                                </a>
+                                            </li>
+                                        )
+                                    })}
+                            </ul>
+                        </div>
+                    </div>
+
+                    <p className="scrolldown">
+                        <a className="smoothscroll" href="#about">
+                            <i className="icon-down-circle" />
+                        </a>
+                    </p>
+                </header>
+            </React.Fragment>
+        )
+    }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,96 @@
+let resumeData = {
+    "imagebaseurl":"https://sebastiansanta.dev/",
+    "name": "Sebastian Santa",
+    "role": "Software Engineer",
+    "linkedinId":"https://www.linkedin.com/in/sebastian-santa/",
+    "twitterid": "cheesecakesanta",
+    "roleDescription": "I enjoy learning about new technologies, challenging myself each day, and playing chess in my freetime.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/sebastian-santa/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/cheesecakeassassin",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"lichess",
+          "url":"https://lichess.org/@/cheesecake_assassin",
+          "className":"lichess"
+        }
+      ],
+    "aboutme":"I am currently a senior at Florida International University and pursuing my Bachelor's in Computer Science from here. I am a recent Full-Stack Web Development graduate from Rutgers Coding Bootcamp.",
+    "address":"Miami, FL",
+    "website":"https://sebastiansanta.dev",
+    "education":[
+      {
+        "UniversityName":"Florida International University",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2022",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Rutgers Coding Bootcamp",
+        "specialization":"Full-Stack Web Development",
+        "MonthOfPassing":"Apr",
+        "YearOfPassing":"2022",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Streamforge",
+        "specialization":"Software Engineer Intern",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2022",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"FinishLine Roofing",
+        "specialization":"Assistant Manager",
+        "MonthOfLeaving":"Apr",
+        "YearOfLeaving":"2022",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Skills I've obtained over the years",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+  }
+  
+  export default resumeData
